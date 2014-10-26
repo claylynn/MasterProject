d9ccc07b8b60e8c2bb13f324d6dd30daeed191cf
claylynn <chen.ling@stonybrook.edu>
2014-10-26 11:40:00 -0400

    Initial Commit

---
+++ story/CSEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSEAppDelegate.h
+//  story
+//
+//  Created by Chen on 10/26/14.
+//  Copyright (c) 2014 sbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ story/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  story
+//
+//  Created by Chen on 10/26/14.
+//  Copyright (c) 2014 sbu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CSEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CSEAppDelegate class]));
+    }
+}
